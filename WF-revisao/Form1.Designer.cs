90ccd20d836aaa42c1146ac4de1a9155a3f39abf
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-03-19 15:27:51 -0300

    Adicionar arquivos de projeto.

---
+++ WF-revisao/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WF_revisao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClique = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClique
+            // 
+            this.btnClique.BackColor = System.Drawing.Color.Lime;
+            this.btnClique.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClique.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClique.Location = new System.Drawing.Point(12, 178);
+            this.btnClique.Name = "btnClique";
+            this.btnClique.Size = new System.Drawing.Size(242, 43);
+            this.btnClique.TabIndex = 0;
+            this.btnClique.Text = "Soma";
+            this.btnClique.UseVisualStyleBackColor = false;
+            this.btnClique.Click += new System.EventHandler(this.btnClique_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtNumero.Location = new System.Drawing.Point(12, 39);
+            this.txtNumero.Multiline = true;
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(242, 39);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero1.Location = new System.Drawing.Point(12, 9);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(148, 18);
+            this.lblNumero1.TabIndex = 2;
+            this.lblNumero1.Text = "Informe o 1°  numero";
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero2.Location = new System.Drawing.Point(12, 92);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(144, 18);
+            this.lblNumero2.TabIndex = 4;
+            this.lblNumero2.Text = "Informe o 2° numero";
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtNumero2.Location = new System.Drawing.Point(12, 122);
+            this.txtNumero2.Multiline = true;
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(242, 39);
+            this.txtNumero2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(266, 233);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnClique);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClique;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.TextBox txtNumero2;
+    }
+}
+
